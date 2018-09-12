2807d794046f48f4b67efa2480f8d432ce9d70a9
Justin Brown <justbrown1980@gmail.com>
2018-09-12 18:46:26 +1000

    Initial Commit
    Commit Basic Site Components and Set up Google Fonts

---
+++ .cache/api-runner-browser-plugins.js
@@ -0,0 +1,7 @@
+module.exports = [{
+      plugin: require('/Users/user/WIP/Furnion/furion/node_modules/gatsby-plugin-offline/gatsby-browser'),
+      options: {"plugins":[]},
+    },{
+      plugin: require('/Users/user/WIP/Furnion/furion/gatsby-browser.js'),
+      options: {"plugins":[]},
+    }]
